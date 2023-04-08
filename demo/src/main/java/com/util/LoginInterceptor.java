deaaa1cb048956965ebf2c532be46c4d5a0eee3f
tagoner <ddamda0101@gmail.com>
2023-04-08 14:14:07 +0900

    커밋 후 STS테스트

---
+++ demo/src/main/java/com/util/LoginInterceptor.java
@@ -0,0 +1,42 @@
+package com.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+			//컨트롤러 진입 전
+			log.info("- PreHanlder -");
+				
+			HttpSession session = request.getSession();
+	        if(session.getAttribute("login") == null) {
+	            log.info("is not login");
+	            
+	        	//로그인 페이지로 이동
+	            response.sendRedirect("/login");
+	            return false;
+	        }
+	        
+	        //Controller 호출
+	        return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		//컨트롤러 진입 후
+		log.info("- PostHanlder -");
+		
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+	
+	
+}
